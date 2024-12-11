0738e249b0a0ad59c0906effcbaab820c323d967
Adwaita25 <115544472+Adwaita25@users.noreply.github.com>
2024-12-11 14:04:34 +0530

    Create 2481. Minimum Cuts to Divide a Circle.py

---
+++ Leetcode/2481. Minimum Cuts to Divide a Circle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 0
+        
+        if n%2 == 0:
+            return n//2
+        else:
+            return n
